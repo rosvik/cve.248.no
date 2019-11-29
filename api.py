6bcb95e55d45f55d433c9c00a02d37510b3745b6
Johannes <j.rosvik@gmail.com>
2019-11-29 23:09:33 +0100

    CVE lookup 1.0

--- api.py
+++ api.py
@@ -1,19 +1,19 @@
-from flask import Flask, g, render_template, jsonify
+from flask import Flask, g, render_template, jsonify, request, Response
 import sqlite3
 import os
 import re
 import urllib
 from markupsafe import Markup
+from werkzeug.routing import BaseConverter
 
 DATABASE = "./cve.db"
 
 # Create app
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 app.config['SECRET_KEY'] = 'super-secret'
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-# helper method to get the database since calls are per thread,
-# and everything function is a new thread when called
 def get_db():
 	db = getattr(g, '_database', None)
 	if db is None:
@@ -22,11 +22,17 @@ def get_db():
 	return db
 
 def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+	d = {}
+	for idx, col in enumerate(cursor.description):
+		d[col[0]] = row[idx]
+	return d
 
+class RegexConverter(BaseConverter):
+	def __init__(self, url_map, *items):
+		super(RegexConverter, self).__init__(url_map)
+		self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
 
 # helper to close
 @app.teardown_appcontext
@@ -38,28 +44,46 @@ def close_connection(exception):
 @app.route("/")
 def index():
 	db = get_db()
-	data = db.execute('SELECT * FROM t WHERE Name = "CVE-1999-0001"').fetchall()
+	data = db.execute('SELECT Name FROM t ORDER BY id DESC limit 100').fetchall()
+	return render_template(r"index.html", title="CVE lookup", data=data)
 
-	return jsonify(data)
+@app.route("/search.json")
+def all_json():
+	query = request.args.get('q')
 
+	formula = "(C|^)(V|^)(E|^)(-|^)([0-9]{1,4}|^)(-|^)[0-9]{4,}$"
+	if not re.search(formula, query):
+		content = {'please move along': 'nothing to see here'}
+		
+		return jsonify({'statuscode': 404, 'message': 'No results found'}), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
-@app.route("/CVE-<int:year>-<int:number>.json")
+	db = get_db()
+	data = db.execute(f'SELECT Name FROM t WHERE Name LIKE \"%{query}%\"').fetchall()
+	return jsonify({'statuscode': 200, 'message': 'success', 'data': data}), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+@app.route('/CVE-<regex("[0-9]+"):year>-<regex("[0-9]+"):number>.json')
 def cve_api(year, number):
 	db = get_db()
 	data = db.execute(f'SELECT * FROM t WHERE Name = "CVE-{year}-{number}"').fetchone()
 
-	return jsonify(data)
+	ret = jsonify(data)
+	resp = Response(response=ret,
+					status=200,
+					mimetype="application/json")
+	return resp
 
-@app.route("/CVE-<int:year>-<int:number>")
-@app.route("/CVE-<int:year>-<int:number>.html")
+@app.route('/CVE-<regex("[0-9]+"):year>-<regex("[0-9]+"):number>')
+@app.route('/CVE-<regex("[0-9]+"):year>-<regex("[0-9]+"):number>.html')
 def cve_frontend(year, number):
 	db = get_db()
 	data = db.execute(f'SELECT * FROM t WHERE Name = "CVE-{year}-{number}"').fetchone()
 
-	data['References'] = data['References'].split('   |   ')
+	if data['References']: data['References'] = data['References'].split('   |   ')
+	if data['Comments']: data['Comments'] = data['Comments'].replace(' | ', ' \n ')
+	if data['Votes']: data['Votes'] = data['Votes'].split(' | ')
 
 	for i, reference in enumerate(data['References']):
-		formula = 'https://'
+		formula = 'http[s]?://'
 		p = re.compile(formula)
 		rep = p.sub(' https://', reference)
 		data['References'][i] = rep
@@ -67,10 +91,4 @@ def cve_frontend(year, number):
 	return render_template(r"cve.html", title=f"CVE-{year}-{number}", data=data)
 
 if __name__ == "__main__":
-	"""
-	Use python sqlite3 to create a local database, insert some basic data and then
-	display the data using the flask templating.
-	
-	http://flask.pocoo.org/docs/0.12/patterns/sqlite3/
-	"""
 	app.run()
--- cve.py
+++ cve.py
@@ -9,65 +9,58 @@ from sqlalchemy.types import NVARCHAR, Integer
 csvfile = "cve.csv"
 table_name = "t"
 sqlite3_location = r"cve.db"
+source_csv = "https://cve.mitre.org/data/downloads/allitems.csv"
 
-
-r = requests.get(
-	"https://cve.mitre.org/data/downloads/allitems.csv", stream=True)
+r = requests.get(source_csv, stream=True)
 
 if r.encoding is None:
 	r.encoding = 'utf-8'
 
 f = open(csvfile, "w")
 
-title = True
 
 dt = pc()
 
+# Init timers array
 dd = [0 for _ in range(6)]
 writestring = ""
+title = True
 
 for i, line in enumerate(r.iter_lines(decode_unicode=True)):
-
 	d = pc()
 	if "** RESERVED **" in line[:50]:
 		continue
 	if "** REJECT **" in line[:50]:
 		continue
-
 	dd[0] = dd[0] + (pc() - d)
-	d = pc()
 
+	d = pc()
 	if i <= 10:
 		if line.endswith(",,,,,"):
 			continue
-
 	dd[1] = dd[1] + (pc() - d)
+	
 	d = pc()
-
 	if title: 
 		title = False
 		f.write('"id",' + line)
 		continue
-
 	dd[2] = dd[2] + (pc() - d)
-	d = pc()
 
+	d = pc()
 	if not i % 10000:
 		print(i)
 		# break
-
 	dd[3] = dd[3] + (pc() - d)
-	d = pc()
 
+	d = pc()
 	writestring += '\n' + str(i) + ',' + line
-
 	dd[4] = dd[4] + (pc() - d)
-	d = pc()
 
+	d = pc()
 	if not i % 100:
 		f.write(writestring)
 		writestring = ''
-
 	dd[5] = dd[5] + (pc() - d)
 
 for i, val in enumerate(dd):
