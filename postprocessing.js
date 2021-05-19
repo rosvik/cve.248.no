c40434d68ff4b7450ee7d024ae0ad5d48bd16e4c
Johannes <j.rosvik@gmail.com>
2021-05-19 16:52:21 +0200

    Update post processing

--- postprocessing.js
+++ postprocessing.js
@@ -1,8 +1,7 @@
 // https://stackoverflow.com/a/61710898/5976426
 
 const p = Deno.run({
-  cmd: ["pip", "install -r requirements.txt"],
-  cmd: ["python3", "cve.py"],
+  cmd: ["./install.sh"],
 });
 
 await p.status();
