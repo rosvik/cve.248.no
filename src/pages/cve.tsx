a862d88fea55e2e5707ed561a1a2f34e8a90260d
Johannes Røsvik <j.rosvik@gmail.com>
2023-02-27 00:18:04 +0100

    add JSON to cve.tsx

--- src/pages/cve.tsx
+++ src/pages/cve.tsx
@@ -6,6 +6,8 @@ import { api } from "../utils/api";
 import { validateUnknown } from "../utils/validator";
 import Link from "next/link";
 
+import Highlight from "react-highlight";
+
 const Home: NextPage = () => {
   const { query } = useRouter();
   const { id } = query;
@@ -111,6 +113,10 @@ const Home: NextPage = () => {
                   </>
                 ))}
               </ul>
+              <h2>JSON</h2>
+              <Highlight language="json">
+                {JSON.stringify(cve.data, null, 2)}
+              </Highlight>
             </>
           ) : (
             <p>{isLoading ? "Loading..." : "No valid CVE found"}</p>
