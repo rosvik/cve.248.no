c3b64e0eba822ebd3d1094838aca75d7e1f1235c
Johannes Røsvik <j.rosvik@gmail.com>
2023-02-26 22:04:01 +0100

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,6 @@ import Link from "next/link";
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
@@ -17,36 +15,25 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+          <h1>cve.248.no</h1>
+          <div>
+            <h3>Examples</h3>
+            <p>
+              <Link className={styles.link} href="/cve?id=CVE-2021-30900">
+                CVE-2021-30900
+              </Link>
+            </p>
+            <p>
+              <Link className={styles.link} href="/cve?id=CVE-2021-40444">
+                CVE-2021-40444
+              </Link>
+            </p>
+            <p>
+              <Link className={styles.link} href="/cve?id=CVE-2022-41099">
+                CVE-2022-41099
+              </Link>
+            </p>
           </div>
-          <p className={styles.showcaseText}>
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-          </p>
         </div>
       </main>
     </>
