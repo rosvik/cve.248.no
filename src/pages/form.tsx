86e34c5b2487dfa4c7638f296ca4f03c1f0aea02
Johannes Røsvik <j.rosvik@gmail.com>
2023-03-13 19:19:56 +0100

    feat: add lookup field

---
+++ src/pages/form.tsx
@@ -0,0 +1,54 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/cve.module.css";
+import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
+
+type Props = {
+  message: string;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async (
+  context
+) => {
+  const { cveId } = context.query;
+  if (!(typeof cveId === "string"))
+    return {
+      props: {
+        message: "Error: Invalid form data",
+      },
+    };
+
+  if (!/^CVE-[0-9]{4}-[0-9]+$/.test(cveId))
+    return {
+      props: {
+        message: "Error: Invalid ID",
+      },
+    };
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: `/${cveId}`,
+    },
+  };
+};
+
+function Page({
+  message,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <>
+      <Head>
+        <title>cve.248.no</title>
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <Link href="/">← Back</Link>
+          <p>{message}</p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Page;
