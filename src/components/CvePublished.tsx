0b618c2108db916319c306325da4894c43563634
Johannes Røsvik <j.rosvik@gmail.com>
2023-03-14 23:27:55 +0100

    feat: add descriptions and Chip component

--- src/components/CvePublished.tsx
+++ src/components/CvePublished.tsx
@@ -1,14 +1,23 @@
-import { Published } from "../types/v5-cve";
+import { Affected, Descriptions, Published } from "../types/v5-cve";
 import styles from "../styles/cve.module.css";
+import { AffectedVersions } from "./AffectedVersions";
+import { Chip } from "./Chip";
 
 export function CveV5Pubished({ cve }: { cve: Published }) {
   const cna = cve.containers.cna;
+  const affectedSystemTypes = getAffectedSystemTypes(cna.affected);
+  console.log(affectedSystemTypes);
   return (
     <>
       <h1>{cve.cveMetadata.cveId}</h1>
-      <p>{`v${cve.dataVersion} ${cve.cveMetadata.state}`}</p>
-      <h2>Description</h2>
-      <p>{cna.descriptions.find((s) => s.lang === "en")?.value}</p>
+      <div className={styles.chipContainer}>
+        {affectedSystemTypes.length > 0 && (
+          <Chip>{affectedSystemTypes.join(" / ")}</Chip>
+        )}
+        <Chip>{cve.cveMetadata.state}</Chip>
+        <Chip>{`${cve.dataVersion}`}</Chip>
+      </div>
+      <p className={styles.description}>{getDescription(cna.descriptions)}</p>
       <h2>References</h2>
       <ul>
         {cna.references.map((r, i) => (
@@ -18,11 +27,44 @@ export function CveV5Pubished({ cve }: { cve: Published }) {
         ))}
       </ul>
       <h2>ASSIGNER</h2>
-      <p>
-        {cve.cveMetadata.assignerShortName} {cve.cveMetadata.assignerOrgId}
-      </p>
+      <p>{cve.cveMetadata.assignerShortName}</p>
       <h2>JSON</h2>
       <pre className={styles.code}>{JSON.stringify(cve, null, 2)}</pre>
     </>
   );
 }
+
+const getDescription = (descriptions: Descriptions) => {
+  const description = descriptions.find((s) =>
+    ["en", "eng", "en-US"].includes(s.lang)
+  )?.value;
+  return description || descriptions[0].value;
+};
+
+/**
+ * Get type of affected products based on CPE. For details, see
+ * https://www.acunetix.com/blog/articles/common-platform-enumeration-cpe-explained/
+ */
+const getAffectedSystemTypes = (affected: Affected): string[] => {
+  const cpes = affected.flatMap((a) => a.cpes);
+  const segments = cpes.map((s) => s?.split(":"));
+  const duplicates = segments.map((s) =>
+    s && s[2]?.length === 1 ? s[2] : null
+  );
+  const affectedTypes = [...new Set(duplicates)];
+
+  return affectedTypes
+    .map((t) => {
+      switch (t) {
+        case "o":
+          return "Operating system";
+        case "h":
+          return "Hardware";
+        case "a":
+          return "Application";
+        default:
+          return undefined;
+      }
+    })
+    .filter(Boolean) as string[];
+};
