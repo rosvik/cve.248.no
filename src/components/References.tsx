848ec3149a72dcef15ccba49d79f91c174916336
Johannes Røsvik <j.rosvik@gmail.com>
2023-07-26 12:37:03 +0200

    refactor references

--- src/components/References.tsx
+++ src/components/References.tsx
@@ -4,37 +4,45 @@ import { Chip } from "./Chip";
 import Image from "next/image";
 
 export function References({ references }: { references: References }) {
+  const openGraphReferences = references.map(
+    (o, i) =>
+      o.openGraphData && (
+        <a key={i} href={o.url} className={styles.open_graph_data}>
+          <p className={styles.url}>{o.url}</p>
+          {o.openGraphData.image && (
+            <aside>
+              <Image
+                src={o.openGraphData.image}
+                width={300}
+                height={200}
+                alt="opengraph image"
+              />
+            </aside>
+          )}
+          <h4>{o.openGraphData.title}</h4>
+          <p>{o.openGraphData.description}</p>
+          {o.tags?.map((tag) => (
+            <Chip key={tag}>{`#${tag}`}</Chip>
+          ))}
+        </a>
+      )
+  );
+
+  const otherReferences = references.map((r, i) =>
+    !r.openGraphData ? (
+      <li key={i}>
+        <a href={r.url}>{r.name ? r.name : r.url}</a>{" "}
+        {r.tags?.map((tag) => (
+          <Chip key={tag}>{`#${tag}`}</Chip>
+        ))}
+      </li>
+    ) : undefined
+  );
+
   return (
-    <ul className={styles.referencesList}>
-      {references.map((r, i) =>
-        r.openGraphData ? (
-          <a key={i} href={r.url} className={styles.open_graph_data}>
-            <p className={styles.url}>{r.url}</p>
-            {r.openGraphData.image && (
-              <aside>
-                <Image
-                  src={r.openGraphData.image}
-                  width={300}
-                  height={200}
-                  alt="opengraph image"
-                />
-              </aside>
-            )}
-            <h4>{r.openGraphData.title}</h4>
-            <p>{r.openGraphData.description}</p>
-            {r.tags?.map((tag) => (
-              <Chip key={tag}>{`#${tag}`}</Chip>
-            ))}
-          </a>
-        ) : (
-          <li key={i}>
-            <a href={r.url}>{r.name ? r.name : r.url}</a>{" "}
-            {r.tags?.map((tag) => (
-              <Chip key={tag}>{`#${tag}`}</Chip>
-            ))}
-          </li>
-        )
-      )}
-    </ul>
+    <>
+      {otherReferences.filter(Boolean).length > 0 && <ul>{otherReferences}</ul>}
+      {openGraphReferences}
+    </>
   );
 }
