e82c5406ea161e41a2c1cf7b43e821a90ca53b56
Johannes Røsvik <j.rosvik@gmail.com>
2023-10-28 21:32:50 +0200

    fix: prevent false positives from hacker news search

--- src/utils/searchHackerNews.ts
+++ src/utils/searchHackerNews.ts
@@ -7,7 +7,7 @@ const HN_API_BASE_URL = env.HN_API_BASE_URL;
 export async function searchHackerNews(
   query: string
 ): Promise<HNSearchResult | undefined> {
-  const res = await fetch(`${HN_API_BASE_URL}search?query=${query}`);
+  const res = await fetch(`${HN_API_BASE_URL}search?query="${query}"`);
   if (!res.ok) return;
   const data = await res.json();
   const hnSearchResult = HNSearchResult.parse(data);
