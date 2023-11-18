2787cf8ece53a5ffe4f8e9217c1a647d111a6481
Johannes Røsvik <j.rosvik@gmail.com>
2023-11-18 16:34:14 +0100

    feat: handle relative image urls when fetching open graph data

--- src/utils/fetch-opengraph-data.ts
+++ src/utils/fetch-opengraph-data.ts
@@ -10,17 +10,31 @@ export async function fetchOpenGraphData(
   url: string
 ): Promise<OpenGraphData | undefined> {
   try {
-    const res = await fetch(url);
+    const srcUrl = new URL(url);
+    const res = await fetch(srcUrl.href);
     if (res.ok) {
       const $ = load(await res.text());
       const title =
         $('meta[property="og:title"]').attr("content")?.trim() || null;
       const description =
         $('meta[property="og:description"]').attr("content")?.trim() || null;
-      const image =
+      let image =
         $('meta[property="og:image"]').attr("content")?.trim() || null;
 
       if (!title && !description && !image) return;
+
+      if (image && !image.startsWith("http")) {
+        try {
+          const imageUrl = new URL(`${srcUrl.origin}${image}`);
+          image = imageUrl.href;
+        } catch (error) {
+          console.error(
+            `Error parsing opengraph image URL ${image} for ${url}:`,
+            error
+          );
+          image = null;
+        }
+      }
 
       return {
         title,
