92bd427dbbfce3e3923a83ee337fb2f8861be552
Johannes Røsvik <j.rosvik@gmail.com>
2024-04-13 16:00:01 +0200

    feat: add favorites to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import {
   InferGetServerSidePropsType,
   type NextPage,
 } from "next";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { ChangeEvent, FormEvent, useState } from "react";
 import { CveLink } from "../components/CveLink";
@@ -10,9 +11,13 @@ import { PageHead } from "../components/PageHead";
 import { prisma } from "../server/db";
 import styles from "../styles/index.module.css";
 import { Published } from "../types/v5-cve";
+import { api } from "../utils/api";
 import { toCve } from "../utils/getCve";
+import { useFavoriteStorage } from "../utils/use-favorite-storage";
 import { clamp } from "../utils/utils";
-import { isPublished } from "../utils/validator";
+import { isPublished, unsafeIsPublished } from "../utils/validator";
+
+const MAX_NUMBER_OF_FAVORITES = 5;
 
 type Props = {
   recents: Published[];
@@ -55,6 +60,7 @@ const Home: NextPage<
 > = ({ recents }) => {
   const [value, setValue] = useState("");
   const router = useRouter();
+  const { favoriteIds } = useFavoriteStorage("favorites");
 
   const onType = (e: ChangeEvent<HTMLInputElement>) => {
     const v = formatValue(e.target.value);
@@ -67,6 +73,13 @@ const Home: NextPage<
       router.push(`/${value}`);
     }
   };
+
+  const favorites = api.prismaRouter.getMany.useQuery({
+    ids:
+      favoriteIds
+        ?.sort((a, b) => b.localeCompare(a))
+        ?.slice(0, MAX_NUMBER_OF_FAVORITES + 2) ?? [],
+  });
 
   return (
     <>
@@ -84,6 +97,20 @@ const Home: NextPage<
               type="text"
             />
           </form>
+          {!!favorites.data?.length && <h3>Favorites</h3>}
+          {favorites.data
+            ?.toReversed()
+            .slice(0, MAX_NUMBER_OF_FAVORITES)
+            .map(
+              (cve) =>
+                unsafeIsPublished(cve.json) && (
+                  <CveLink key={cve.id} cve={cve.json} />
+                )
+            )}
+          {!!favorites.data?.length &&
+            favorites.data?.length > MAX_NUMBER_OF_FAVORITES && (
+              <Link href="/favorites">All favorites →</Link>
+            )}
           <h3>Recent</h3>
           {recents?.map(
             (cve) =>
