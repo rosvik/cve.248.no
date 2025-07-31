c5d51ab805bce0946ae0771c517029ad4d85c9c6
Johannes Røsvik <j.rosvik@gmail.com>
2025-08-01 00:34:32 +0200

    fix: don't mutate favorites when reversing the array

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -21,7 +21,7 @@ function Page({}) {
           <h1>Favorite CVEs</h1>
           {cves.isLoading && <p>Loading...</p>}
           {cves.data
-            ?.reverse()
+            ?.toReversed()
             .map(
               (cve) =>
                 cve.cveMetadata.state === "PUBLISHED" && (
